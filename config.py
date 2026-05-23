85a65d850ba02603d4b1590624f6a70b5d64b0cf
linkinpark1234 <154906244+linkinpark1234@users.noreply.github.com>
2026-05-24 01:27:56 +0530

    Update secret keys to longer random values

--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 class Config:
-    SECRET_KEY = "supersecretkey"
-
+    SECRET_KEY = "this-is-a-long-random-secret-key-32chars!"
     SQLALCHEMY_DATABASE_URI = "sqlite:///expenses.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    JWT_SECRET_KEY = "jwt-secret-key"
+    JWT_SECRET_KEY = "this-is-a-long-jwt-secret-key-32chars!!"
